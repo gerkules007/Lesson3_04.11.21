1bc3a9ffd126b1dd4b2f97840bc27efc2c598082
OvcS <gerkules007@yahoo.com>
2021-11-04 21:46:01 +0300

    Create Task008

---
+++ Task008/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите число (лучше положительное)");
+int a = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < a + 1; i++) if (i % 2 == 0) { Console.Write((i) + " "); }
