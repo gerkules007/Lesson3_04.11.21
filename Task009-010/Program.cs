07144eb48d5eff2ae8aca7fdb74f559f2f808c52
OvcS <gerkules007@yahoo.com>
2021-11-04 22:07:33 +0300

    Create Task009-010

---
+++ Task009-010/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a % 10;
+Console.WriteLine($"Последняя цифра трехзначного числа {b}");
+int c = a % 100 /10;
+Console.WriteLine($"Вторая цифра трехзначного числа {c}");
