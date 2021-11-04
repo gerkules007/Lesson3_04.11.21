c52b905475b3c29cbbc26105d870e04b955dedf4
OvcS <gerkules007@yahoo.com>
2021-11-04 21:23:50 +0300

    Create Task005

---
+++ Task005/Program.cs
@@ -0,0 +1,8 @@
+﻿int Function(int x)
+{
+    int y = x * x + 10;
+    return y;
+}
+Console.WriteLine("Введите число x");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Ваш ответ {Function(a)}");
