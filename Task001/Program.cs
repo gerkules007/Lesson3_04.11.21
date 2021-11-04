41932808b19fddfbcf21849fdeffa7a984ab0649
OvcS <gerkules007@yahoo.com>
2021-11-04 20:49:12 +0300

    Create Task001

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -3,5 +3,4 @@ int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число второе число");
 int b = Convert.ToInt32(Console.ReadLine());
 if (b == Math.Pow(a, 2)) Console.WriteLine($"Число {(int) a} является квадратом второго числа {(int) b}");
-else 
-Console.WriteLine("Не является");
+else {Console.WriteLine("Не является");}
