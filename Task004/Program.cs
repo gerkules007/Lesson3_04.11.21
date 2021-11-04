1eaa5e27816ec790004846fd0a9f3d64df9223db
OvcS <gerkules007@yahoo.com>
2021-11-04 21:17:30 +0300

    Create Task004

---
+++ Task004/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[3];
+array[0] = a;
+array[1] = b;
+array[2] = c;
+
+(int min, int max) FindMinMax(int[] input)
+{
+    if (input is null || input.Length == 0)
+    {
+        throw new ArgumentException("Cannot find minimum and maximum of a null or empty array.");
+    }
+
+    var min = int.MaxValue;
+    var max = int.MinValue;
+    foreach (var i in input)
+    {
+        if (i < min)
+        {
+            min = i;
+        }
+        if (i > max)
+        {
+            max = i;
+        }
+    }
+    return (min, max);
+}
+var result = FindMinMax(array);
+Console.WriteLine($"из всех чисел минимальное = {result.min}, максимальное = {result.max}");
