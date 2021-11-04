98891136b39671718546a6790ac58954a98522de
OvcS <gerkules007@yahoo.com>
2021-11-04 20:52:23 +0300

    Create Task002

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.WriteLine("Введите число, которое нужно проверять");
 int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число второе число");
+Console.WriteLine("Введите второе число");
 int b = Convert.ToInt32(Console.ReadLine());
 if (b == Math.Pow(a, 2)) Console.WriteLine($"Число {(int) a} является квадратом второго числа {(int) b}");
 else {Console.WriteLine("Не является");}
---
+++ Task002/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b) Console.WriteLine($"Число {(int) a} больше числа {(int) b}");
+else {Console.WriteLine($"Число {(int) a} меньше или равно числу {(int) b}");}
+
