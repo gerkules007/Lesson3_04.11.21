7f6020ec0d3cdecf1d84a04a5bbc8037ed4b5500
OvcS <gerkules007@yahoo.com>
2021-11-04 21:30:07 +0300

    Create Task006

---
+++ Task006/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a %2 == 0) Console.WriteLine($"Число {(int) a} является четным");
+else {Console.WriteLine($"Число {(int) a} не является четным");}
