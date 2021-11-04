380c08f43feb047b97d50b23b52efb84d14ccae4
OvcS <gerkules007@yahoo.com>
2021-11-04 21:39:09 +0300

    Create Task007

---
+++ Task007/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите отрицательное число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите положительное число");
+int b = Convert.ToInt32(Console.ReadLine());
+int count = (-a) + b;
+for (int i = 0; i < count+1; Console.Write((a) + " "), a++, i++);
