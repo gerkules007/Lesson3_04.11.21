b72927cbecab9092e5e22dd1829fd12d370a7d98
OvcS <gerkules007@yahoo.com>
2021-11-09 16:33:33 +0300

    Task013 Done

--- Task013/Program.cs
+++ Task013/Program.cs
@@ -4,4 +4,4 @@ int b = 200;
 int c = b % a;
 Console.WriteLine();
 if (c == 0) Console.WriteLine($"Введенное число кратно начальному");
-else { Console.WriteLine($"Число не кратно, введите новое, полученный остаток {c}"); }
+else { Console.WriteLine($"Число не кратно, введите новое, полученный остаток {c}");}
