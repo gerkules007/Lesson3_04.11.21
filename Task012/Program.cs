cfc18560f13a9bec1746d6fb25c61fdc59be112a
OvcS <gerkules007@yahoo.com>
2021-11-09 16:17:38 +0300

    Task012 Done

---
+++ Task012/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a % 10;
+int c = a % 1000 / 100;
+int result = c * 100 + b;
+Console.WriteLine($"Преобразованное число {result}");
