320ea5a74f23997de8a6016ad5bfae39bf93343e
OvcS <gerkules007@yahoo.com>
2021-11-09 16:29:57 +0300

    Task013 Done

---
+++ Task013/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = 200;
+int c = b % a;
+Console.WriteLine();
+if (c == 0) Console.WriteLine($"Введенное число кратно начальному");
+else { Console.WriteLine($"Число не кратно, введите новое, полученный остаток {c}"); }
