d561a830708509c5a70f7cca1276603a7d5e3349
OvcS <gerkules007@yahoo.com>
2021-11-09 19:18:02 +0300

    Task014 Remastered

--- Task014/Program.cs
+++ Task014/Program.cs
@@ -1,25 +1,17 @@
-﻿int RangeNumber(int num)
+﻿void Compare3(int n)
 {
-    int k = 0;
-    while (num != 0)
+    if (100 <= n)
     {
-        k++;
-        num = num / 10;
+        int b = n % 1000 / 100;
+        Console.WriteLine($"Третья цифра числа равна {b}");
     }
-    return k;
-}
-
-void Compare3(int n, int count)
-{
-    if (count < 3) Console.WriteLine($"Число меньше трехзначного");
     else
     {
-        int c = n / 100;
-        Console.WriteLine($"Третья цифра числа равна {c}");
+        Console.WriteLine($"Число меньше трехзначного");
     }
 }
 
 Console.WriteLine("Введите число");
 int a = Convert.ToInt32(Console.ReadLine());
-int b = RangeNumber(a);
-Compare3(a, b);
+Compare3(a);
+Console.WriteLine();
