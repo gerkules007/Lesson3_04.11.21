dcb5d5e200c7791e15541ec9fcb490793f276efa
OvcS <gerkules007@yahoo.com>
2021-11-10 12:56:40 +0300

    Create structure multiprogramm

--- Multitask/Program.cs
+++ Multitask/Program.cs
@@ -1,48 +1,104 @@
-﻿// Методы для работы
-// Метод свитч
-/*
-    Начальные аргуметны
-    Постепенное выполнение методов
-    Вызов метода печати
-*/
-
-// Основная функция
-/*
+﻿/* Cтруктура программы
 Приветствие
-Выбор функций
-Защита от случаный нажатий
-Подключение значений
-Расчет
-Вывод на экран
+Ввод команды
+    Ввод числа
+        Выбор задачи
+            Методы решения задачи
+    Ввод символа
+        Помощь
+        Выход
+        Ошибка если нет символов
+Печать полученного решения на экран
 */
 
-// Алгоритм:
-// Используем вводы данных
-// Используем присваиваемые значения
-// Рассчитываем значения через методы
-
-// Метод ввода данных
-void InputData(string[] array1, int[] array2, int i)
+// Метод ввода номера программы
+void Enterprogram()
 {
-    int input;
+    Console.WriteLine("Чтобы вызвать помощь в строке введите 'h'");
+    Console.WriteLine();
+    System.Threading.Thread.Sleep(1000);
+    bool exit = false;
+    while (exit == false)
+    {
+        Console.WriteLine("Введите команду");
+        string input = Console.ReadLine()!;
+        System.Threading.Thread.Sleep(1000);
+        bool check = int.TryParse(input, out int number);
+
+        switch (check)
+        {
+            case true:
+                Console.Write("Загружаю");
+                System.Threading.Thread.Sleep(500);
+                Console.Write(".");
+                System.Threading.Thread.Sleep(500);
+                Console.Write(".");
+                System.Threading.Thread.Sleep(500);
+                Console.Write(".");
+                Console.WriteLine();
+                ChooseProgramm(input);
+                Console.WriteLine();
+                break;
+            case false:
+                switch (input)
+                {
+                    case "q":
+                        exit = true;
+                        break;
+                    case "h":
+                        Console.WriteLine("Чтобы включить программу введите ее номер и нажмите Enter. Например: Для программы под номером 20, введите '20'");
+                        break;
+                    default:
+                        Console.WriteLine("Ошибка, повторите запрос");
+                        break;
+                }
+                break;
+        }
+    }
+}
+
+// Метод выбора решения программы по введенному значению
+void ChooseProgramm(string position)
+{
+    switch (position)
+    {
+        case "0":
+            string[] text = { "Введите первое число", "Введите второе число" };
+            double[] arr = CreateArrayInt32(text);
+            for (int i = 0; i < text.Length; i++) InputData(text, arr, i);
+            Console.WriteLine(string.Join(" ", arr));
+            break;
+        default:
+            Console.WriteLine("Такой задачи не существует, попробуйте еще раз");
+            break;
+    }
+}
+
+// Метод ввода данных в вычисляемый массив (проверяет на правильность ввода данных)
+void InputData(string[] array1, double[] array2, int i)
+{
+    double input;
     do
     {
         Console.WriteLine(array1[i]);
-    } while (!Int32.TryParse(Console.ReadLine(), out input));
+    } while (!Double.TryParse(Console.ReadLine(), out input));
     array2[i] = input;
 }
 
-int[] CreateArrayInt32(string[] array1)
+double[] CreateArrayInt32(string[] array1)
 {
-    int[] array2 = { -1 };
+    double[] array2 = { -1 };
     int count = array1.Length;
     Array.Resize(ref array2, count);
     return array2;
 }
 
-string[] text = { "Введите первое число", "Введите второе число" };
-int[] arr = CreateArrayInt32(text);
-for (int i = 0; i < text.Length; i++) InputData(text, arr, i);
-Console.WriteLine(string.Join(" ", arr));
-Array.ConvertAll<int,double>(arr, x => x);
-//var d = i.Select(x => (double)x).ToArray();
+// Main
+Console.WriteLine("Введите свое имя");
+var name = Console.ReadLine()!;
+Console.Clear();
+System.Threading.Thread.Sleep(1000);
+Console.WriteLine($"Приветствую тебя, {name}");
+System.Threading.Thread.Sleep(2000);
+Enterprogram();
+
